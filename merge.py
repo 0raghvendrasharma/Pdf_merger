813ee233f5e68165ff1f2738648244f4590f3ac4
Raghvendra Sharma <raghvendrasharma1122@gmail.com>
2024-03-12 18:05:59 +0530

    tested and got a merged pdf

--- merge.py
+++ merge.py
@@ -1,10 +1,10 @@
 import PyPDF2
 
-pdfiles = ["1.pdf", "2.pdf"]
+pdfiles = ["1.pdf.pdf", "2.pdf.pdf"]
 merger = PyPDF2.PdfMerger()
 for filename in pdfiles:
     pdfFile = open(filename, 'rb')
-    pdfReader = PyPDF2.PdfFileReader(pdfFile)
+    pdfReader = PyPDF2.PdfReader(pdfFile)
     merger.append(pdfReader)
 
 pdfFile.close()
